1942db0c0834a5d2ae75088a197ce20df3c02424
Admin <Admin@LAPTOP-I8JGD7GF>
2021-04-26 19:23:51 +0900

    업비트 모델 추가

---
+++ src/main/java/com/nice/demo/upbit/model/UpbitOrderRequest.java
@@ -0,0 +1,19 @@
+package com.nice.demo.upbit.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UpbitOrderRequest {
+
+    private String market;
+
+    private String side;
+
+    private String volume;
+
+    private String price;
+
+    private String ord_type;
+}
---
+++ src/main/java/com/nice/demo/upbit/model/UpbitTrade.java
@@ -0,0 +1,49 @@
+package com.nice.demo.upbit.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class UpbitTrade {
+
+    private String code;
+
+    @JsonProperty("trade_volume")
+    private BigDecimal tradeVolume;
+
+    @JsonProperty("trade_time")
+    private String tradeTime;
+
+    @JsonProperty("trade_price")
+    private BigDecimal tradePrice;
+
+    @JsonProperty("sequential_id")
+    private BigDecimal sequentialId;
+
+    @JsonProperty("trade_timestamp")
+    private Long tradeTimestamp;
+
+    private String change;
+
+    private String type;
+
+    @JsonProperty("trade_date")
+    private String tradeDate;
+
+    @JsonProperty("stream_type")
+    private String streamType;
+
+    @JsonProperty("prev_closing_price")
+    private BigDecimal prevClosingPrice;
+
+    @JsonProperty("change_price")
+    private BigDecimal changePrice;
+
+    @JsonProperty("ask_bid")
+    private String askBid;
+
+    private Long timestamp;
+}
